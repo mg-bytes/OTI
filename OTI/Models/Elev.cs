3218561e3d37be9e336408b49014b4b458afbf6c
George Balota <gxg.bl99@gmail.com>
2025-11-15 17:42:14 +0200

    Initial commit

---
+++ OTI/Models/Elev.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTI.Models
+{
+    public class Elev
+    {
+        public string Nume { get; set; }
+        public string Prenume { get; set; }
+        public string Clasa { get; set; }
+        public int Varsta { get; set; }
+        public string Email { get; set; }
+        public string Telefon { get; set; }
+        public List<Materie> Materii { get; set; }
+    }
+}
---
+++ OTI/Models/Materie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTI.Models
+{
+    public class Materie
+    {
+        public string Denumire { get; set; }
+        public List<int> Note { get; set; }
+    }
+}
